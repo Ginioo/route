0b93a0d937a2f1ad6281a6f582769226dd624fb9
gino <ginowu@taiwanmobile.com>
2016-01-12 10:55:49 +0000

    commit

--- Application.php
+++ Application.php
@@ -43,7 +43,6 @@ class Application extends Event
                 $eventName = '';
                 break;
         }
-
         return $eventName . $requestUri;
     }
 
--- TestController.php
+++ TestController.php
@@ -18,5 +18,31 @@ class TestController
     public function test2($input)
     {
         echo __CLASS__ . '::' . __FUNCTION__;
+        echo $path = dirname(dirname(dirname(__DIR__))) . '/EstBadminton/view/index.html';
+        require($path);
+    }
+
+    public function index($input)
+    {
+        $path = dirname(dirname(dirname(__DIR__))) . '/EstBadminton/view/index.html';
+        require($path);
+    }
+
+    public function member($input)
+    {
+        $path = dirname(dirname(dirname(__DIR__))) . '/EstBadminton/view/member.html';
+        require($path);
+    }
+
+    public function passbook($input)
+    {
+        $path = dirname(dirname(dirname(__DIR__))) . '/EstBadminton/view/passbook.html';
+        require($path);
+    }
+
+    public function admin($input)
+    {
+        $path = dirname(dirname(dirname(__DIR__))) . '/EstBadminton/view/admin.html';
+        require($path);
     }
 }
